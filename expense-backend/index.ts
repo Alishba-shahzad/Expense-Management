961c82aa4a54cd769e03ab00ef6ab6c40f88d581
Alishba-shahzad <alishbah722@gmail.com>
2025-07-23 23:49:05 +0500

    Update index.ts

--- expense-backend/index.ts
+++ expense-backend/index.ts
@@ -7,7 +7,7 @@ import expenseRoutes from "./routes/expenses";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 // Middleware
 app.use(cors());
