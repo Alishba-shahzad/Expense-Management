5c4b1d19ff5409409ec5223db45f6a5fa1df5bbf
Alishba-shahzad <alishbah722@gmail.com>
2025-07-25 23:28:44 +0500

    Update index.ts

--- expense-backend/index.ts
+++ expense-backend/index.ts
@@ -7,7 +7,7 @@ import expenseRoutes from "./routes/expenses";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -24,11 +24,11 @@ app.use("/api/expenses", expenseRoutes);
 // MongoDB connect
 mongoose.connect(process.env.MONGO_URI!)
   .then(() => {
-    console.log("✅ MongoDB connected");
+    console.log(" MongoDB connected");
     app.listen(PORT, () => {
       console.log(` Server running on http://localhost:${PORT}`);
     });
   })
   .catch((err) => {
-    console.error("❌ MongoDB connection failed:", err);
+    console.error(" MongoDB connection failed:", err);
   });
