12aa873d3508ef88b333cb3c697db048eb161d22
Alishba-shahzad <alishbah722@gmail.com>
2025-08-11 23:17:10 +0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,24 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
-import { getAuth,GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// import { getAuth,GoogleAuthProvider } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDBbsDsentA8m1FMVpIOUOt08Q9YNdlEQA",
-  authDomain: "expense-7.firebaseapp.com",
-  projectId: "expense-7",
-  storageBucket: "expense-7.firebasestorage.app",
-  messagingSenderId: "280453535513",
-  appId: "1:280453535513:web:b16898487b321b4970d788",
-  measurementId: "G-LD1RP18S6K"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDBbsDsentA8m1FMVpIOUOt08Q9YNdlEQA",
+//   authDomain: "expense-7.firebaseapp.com",
+//   projectId: "expense-7",
+//   storageBucket: "expense-7.firebasestorage.app",
+//   messagingSenderId: "280453535513",
+//   appId: "1:280453535513:web:b16898487b321b4970d788",
+//   measurementId: "G-LD1RP18S6K"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
 
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider(); 
-export const db = getFirestore(app);
+// export const auth = getAuth(app);
+// export const googleProvider = new GoogleAuthProvider(); 
+// export const db = getFirestore(app);
