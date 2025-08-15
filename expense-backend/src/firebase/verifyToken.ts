1a9a8f61868ef9d9817dc0390252856cc9d4f8a5
Alishba-shahzad <alishbah722@gmail.com>
2025-08-15 19:18:49 +0500

    Update verifyToken.ts

--- expense-backend/src/firebase/verifyToken.ts
+++ expense-backend/src/firebase/verifyToken.ts
@@ -1,33 +1,33 @@
-// import { Request, Response, NextFunction } from "express";
-// import admin from "./firebaseAdmin";
+import { Request, Response, NextFunction } from "express";
+import admin from "./firebaseAdmin";
 
-// export interface AuthenticatedRequest extends Request {
-//   user?: admin.auth.DecodedIdToken;
-// }
+export interface AuthenticatedRequest extends Request {
+  user?: admin.auth.DecodedIdToken;
+}
 
-// const verifyToken = async ( 
-//   req: AuthenticatedRequest,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const authHeader = req.headers.authorization;
+const verifyToken = async ( 
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
 
-//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//     return res.status(401).json({ error: "Unauthorized: No token" });
-//   }
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized: No token" });
+  }
 
-//   const token = authHeader.split("Bearer ")[1];
+  const token = authHeader.split("Bearer ")[1];
 
-//   try {
-//     const decoded = await admin.auth().verifyIdToken(token);
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
 
-//     console.log("Token verified");
-//     console.log("UID:", decoded.uid);
-//     console.log("Email:", decoded.email);
+    console.log("Token verified");
+    console.log("UID:", decoded.uid);
+    console.log("Email:", decoded.email);
 
-//     req.user = decoded;
-//     next();
-//   } catch (err) {
+    req.user = decoded;
+    next();
+  } catch (err) {
     return res.status(401).json({ error: "Unauthorized: Invalid token" });
   }
 };
